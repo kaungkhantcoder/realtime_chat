b26e2a8505bc262a208a53e32a0219a55ff7b65c
kaungkhantcoder <kaungkhantcoder@gmail.com>
2026-02-02 22:46:32 +0630

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { client } from "@/lib/client";
 import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 const ANIMALS = ["wolf", "hawk", "bear", "shark"];
 const STORAGE_KEY = "chat_username";
@@ -14,6 +15,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const router = useRouter()
 
   useEffect(() => {
     const main = () => {
@@ -34,6 +36,10 @@ export default function Home() {
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
       const res = await client.room.create.post();
+
+      if(res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`)
+      }
     },
   });
 
